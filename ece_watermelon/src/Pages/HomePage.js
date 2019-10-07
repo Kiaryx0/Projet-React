98c71af35fe6388e4c7ef8acad52d26790ed8c36
Louis <louisdeveze78@gmail.com>
2019-10-07 12:30:09 +0200

    commit Carousel

--- ece_watermelon/src/Pages/HomePage.js
+++ ece_watermelon/src/Pages/HomePage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import HomeNavBar from './Components/HomeNavBar'
+import HomeCarousel from './Components/HomeCarousel';
 
 
 const HomePage = () => (
+    
+    
     <div>
-        <HomeNavBar></HomeNavBar>
+        <header><HomeNavBar/></header>
+        <div>
+            <HomeCarousel/>
+        </div>        
     </div>
-);
-
-
-
-export default HomePage;
+    );
+    
+    
+    export default HomePage;
+    
