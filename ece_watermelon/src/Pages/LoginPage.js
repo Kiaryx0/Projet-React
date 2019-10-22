705da67c15dc420f52724d5a4aad0e8d0937b7e0
Louis <louisdeveze78@gmail.com>
2019-10-22 13:42:20 +0200

    up

--- ece_watermelon/src/Pages/LoginPage.js
+++ ece_watermelon/src/Pages/LoginPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import "./acss.css";
-import HomeNavBar from './Components/HomeNavBar.js';
-import LoginForm from './Components/Forms/LoginForm.js';
-
 
 const LoginPage = () => (
-    <div className="background">
-        <header><HomeNavBar/></header>
-        <div><LoginForm/></div>
+    <div>
+        <h1>LoginPage</h1>
     </div>
 );
 
--- ece_watermelon/src/Pages/RegisterPage.js
+++ ece_watermelon/src/Pages/RegisterPage.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import HomeNavBar from './Components/HomeNavBar.js';
-import RegisterForm from './Components/Forms/RegisterForm.js';
-import "./acss.css";
 
 const RegisterPage = () => (
-    <div className="background">
-        <header><HomeNavBar/></header>
-        <div><RegisterForm/></div>
+    <div>
+        <h1>RegisterPage</h1>
     </div>
 );
 
